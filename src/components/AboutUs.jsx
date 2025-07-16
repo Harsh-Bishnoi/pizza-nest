44a9c0e765c2adb137d74419a4de4ecd43b35bf7
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-07-17 01:31:55 +0530

    Refactor AboutUs component to replace gradient border with dashed border for improved styling; remove unused gradient-border CSS

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -24,7 +24,7 @@ const AboutUs = () => {
               <div className="flex flex-wrap mt-6 gap-4 sm:gap-6 lg:max-h-[135px] max-lg:justify-center">
                 {ABOUT_US_CARD_DATA.map((item, index) => (
                   <div className={`${index === 1 ? "sm:mt-3" : ""}`}>
-                    <div key={index} className="bg-[#FEFEFE] relative gradient-border w-full min-w-[160px] sm:max-w-[170px] sm:min-w-[170px] p-3 sm:p-4.5 shadow-[0px_1px_7px_0px_#F67A211F] rounded-xl">
+                    <div key={index} className="bg-[#FEFEFE] border border-dashed border-[#f67a21]/50 w-full min-w-[160px] sm:max-w-[170px] sm:min-w-[170px] p-3 sm:p-4.5 shadow-[0px_1px_7px_0px_#F67A211F] rounded-xl">
                       <div className="bg-[#FCECE2] size-[52px] mx-auto rounded-full flex justify-center items-center">{<item.img />}</div>
                       <p className='leading-[160%] text-center whitespace-nowrap text-[#474747] pt-2'>{item.title}</p>
                     </div>
