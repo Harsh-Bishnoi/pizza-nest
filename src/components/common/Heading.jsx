25b9692fdb09a9288694a593d4c601cf6e0b3e09
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-07-17 09:34:21 +0530

    change in heading

--- src/components/common/Heading.jsx
+++ src/components/common/Heading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Heading = ({ headingClass, headingText,spanText }) => {
     return (
-        <h2 className={`text-3xl sm:text-5xl font-semibold leading-[120%] text-black ${headingClass}`}>{headingText} <span className=''>{ spanText}</span></h2>
+        <h2 className={`text-3xl sm:text-[40px] md:text-5xl font-semibold leading-[120%] text-black ${headingClass}`}>{headingText} <span className=''>{ spanText}</span></h2>
     )
 }
 
